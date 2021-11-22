9c06a65d420cdc1904b2189649216def9ed077b4
rusydeveloper <rusy.developer@gmail.com>
2021-11-22 08:23:14 +0700

    updated link

--- src/views/CoopProfile.vue
+++ src/views/CoopProfile.vue
@@ -1,25 +1,40 @@
 <template>
-<v-container fluid class="pa-0">
-  <v-banner>
-    <v-btn x-large to="/">
-    <v-icon left>
-      mdi-arrow-left
-    </v-icon>
-    KEMBALI
-  </v-btn>
-  </v-banner>
-  
-  <v-img :src="require('../assets/logo-kota-bandung.png')" class="my-3" contain height="120" />
-  <div>
-    DINAS KOPERASI DAN UMKM KOTA BANDUNG
-    <br/>
-    Profil Koperasi
-  </div>
-  <v-btn x-large color="warning" href="https://gov.nectico.com/main" target="_blank" dark>
-    Kunjungi Website untuk Selengkapnya
-  </v-btn>
-  <v-row align="center" justify="space-around">
-    <v-img :src="require('../assets/CoopProfileBackground.png')" class="my-3" contain />
-  </v-row>
-</v-container>
+  <v-container fluid class="pa-0">
+    <v-banner>
+      <v-btn x-large to="/">
+        <v-icon left>
+          mdi-arrow-left
+        </v-icon>
+        KEMBALI
+      </v-btn>
+    </v-banner>
+
+    <v-img
+      :src="require('../assets/logo-kota-bandung.png')"
+      class="my-3"
+      contain
+      height="120"
+    />
+    <div>
+      DINAS KOPERASI DAN UMKM KOTA BANDUNG
+      <br />
+      Profil Koperasi
+    </div>
+    <v-btn
+      x-large
+      color="warning"
+      href="https://siapik-kelembagaan.netlify.app/"
+      target="_blank"
+      dark
+    >
+      Kunjungi Website untuk Selengkapnya
+    </v-btn>
+    <v-row align="center" justify="space-around">
+      <v-img
+        :src="require('../assets/CoopProfileBackground.png')"
+        class="my-3"
+        contain
+      />
+    </v-row>
+  </v-container>
 </template>
